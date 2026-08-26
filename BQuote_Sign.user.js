8cf19ea9043086b808f28cedebf2bfe8eae29be2
Personwritep <89448434+personwritep@users.noreply.github.com>
2026-08-26 19:07:54 +0900

    Update version to 1.2 and adjust match/exclude rules
    Updated version number and modified match/exclude rules.

--- BQuote_Sign.user.js
+++ BQuote_Sign.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        BQuote Sign ⭐
 // @namespace        http://tampermonkey.net/
-// @version        1.1
+// @version        1.2
 // @description        引用部「blockquote」をデザインする「Ctrl+F2」
 // @author        Ameba Blog User
-// @match        https://blog.ameba.jp/ucs/entry/srventry*
-// @exclude        https://blog.ameba.jp/ucs/entry/srventrylist.do*
+// @match        https://blog.ameba.jp/ucs/entry/srventryinsertinput.do*
+// @match        https://blog.ameba.jp/ucs/entry/srventryupdateinput.do*
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=ameblo.jp
 // @grant        none
 // @updateURL        https://github.com/personwritep/BQuote_Sign/raw/main/BQuote_Sign.user.js
